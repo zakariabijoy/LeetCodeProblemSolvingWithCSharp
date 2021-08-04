3998171338ca73ba1b9726f5106c4b41040d8462
zakariabijoy <zakaria.bijoy@live.com>
2021-08-04 22:10:52 +0600

    Add project files.

---
+++ TwoSum/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace TwoSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           var result = TwoSum(new int[] { 2, 7, 11, 15 }, 9);
+            Console.WriteLine($"[{ result[0]},{result[1]}]");
+        }
+
+
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            int[] a = new int[2];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length; j++)
+                {
+                    if (i == j)
+                        continue;
+                    else
+                    {
+                        if (nums[i] + nums[j] == target)
+                        {
+                            a[0] = i;
+                            a[1] = j;
+                        }
+                    }
+                }
+            }
+
+
+            return a;
+        }
+    }
+}
