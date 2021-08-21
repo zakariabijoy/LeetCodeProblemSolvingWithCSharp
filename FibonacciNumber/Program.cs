ce2176b8676660c18cb8c478858c8d20476e2eaf
zakariabijoy <zakaria.bijoy@live.com>
2021-08-22 05:21:51 +0600

    Fibonacci Number problem solved

---
+++ FibonacciNumber/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FibonacciNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Fib(2);
+        }
+
+        public static int Fib(int n)
+        {
+            if (n == 0)
+                return 0;
+            if (n == 1)
+                return 1;
+
+            return Fib(n - 1) + Fib(n - 2);
+
+        }
+       
+    }
+}
---
+++ ValidParentheses/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ValidParentheses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IsValid("s");
+        }
+
+        public static bool IsValid(string s)
+        {
+            return true;
+        }
+    }
+}
