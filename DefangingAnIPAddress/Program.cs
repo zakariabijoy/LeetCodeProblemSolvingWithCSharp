3495fab5b8066484365a345bbec6c726a63cc4d1
zakariabijoy <zakaria.bijoy@live.com>
2021-08-27 02:22:57 +0600

    Defanging an IP Address problem solved

---
+++ DefangingAnIPAddress/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace DefangingAnIPAddress
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine( DefangIPaddr("1.1.1.1"));
+        }
+
+
+        public static string DefangIPaddr(string address)
+        {
+            string ip = string.Empty;
+
+            for (int i = 0; i < address.Length; i++)
+            {
+                if(address[i] == '.')
+                {
+                    ip = $"{ip}[{address[i]}]";
+                }
+                else
+                {
+                    ip = $"{ip}{address[i]}";
+                }
+                
+            }
+
+            return ip;
+        }
+    }
+}
