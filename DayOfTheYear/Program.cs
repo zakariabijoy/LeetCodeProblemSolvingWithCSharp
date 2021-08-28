e7a63158af94b443254925ea7455075276509346
zakariabijoy <zakaria.bijoy@live.com>
2021-08-29 01:56:59 +0600

    DayOfTheYear Problem solved

---
+++ DayOfTheYear/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DayOfTheYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DayOfYear("2019-01-09"));
+        }
+
+        public static int DayOfYear(string date)
+        {
+           return DateTime.Parse(date).DayOfYear;
+        }
+    }
+}
