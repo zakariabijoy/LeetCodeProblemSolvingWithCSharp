90f38df93e36e7bbfeadf3829a46505603cb7f2b
zakariabijoy <zakaria.bijoy@live.com>
2021-08-29 03:53:23 +0600

    Palindrome Number Problem solved

---
+++ PalindromeNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PalindromeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(IsPalindrome(121));
+        }
+
+        public static bool IsPalindrome(int x)
+        {
+            var val = x.ToString();
+            string result = string.Empty;
+
+            for (int i = val.Length -1 ; i < val.Length; i--)
+            {
+                if (i >= 0)
+                {
+                    result = $"{result}{val[i]}";
+                }
+                else
+                {
+                    break;
+                }
+
+                
+            }
+
+           
+
+            if (x.ToString() == result) return true;
+
+            return false;
+        }
+    }
+}
